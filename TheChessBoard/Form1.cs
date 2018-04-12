4189850875697dea6b52f16a9890c4e1d5fe9063
actprophet <2838955213@qq.com>
2018-04-12 23:34:05 +0800

    我简单的写了个类

--- TheChessBoard/Form1.cs
+++ TheChessBoard/Form1.cs
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace TheChessBoard
 {
+    public class MyChess
+    {
+        public Label label;
+        public char name;
+        public char x;
+        public int y;
+
+        public void Initialization(Label l, char c, char j, char k)
+        {
+            label = l;
+            name = c;
+            x = j;
+            y = k;
+        }
+    }
     public partial class Form1 : Form
     {
+        
+        MyChess chesses = new MyChess();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
