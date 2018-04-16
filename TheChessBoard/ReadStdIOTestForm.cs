790c6917937536f35c1cc94835c36fe63fe63d21
Shun <shun1048576@zoho.com>
2018-04-16 15:29:17 +0800

    Restart processes when click buttons again

--- TheChessBoard/ReadStdIOTestForm.cs
+++ TheChessBoard/ReadStdIOTestForm.cs
@@ -23,21 +23,6 @@ namespace TheChessBoard
 
         private void ReadStdIOTestForm_Load(object sender, EventArgs e)
         {
-            proc1 = new Process();
-            proc2 = new Process();
-            proc1.StartInfo.FileName = @"..\TestConsoleApp1\bin\Debug\TestConsoleApp1.exe";
-            proc1.StartInfo.UseShellExecute = false;
-            proc1.StartInfo.RedirectStandardInput = true;
-            proc1.StartInfo.RedirectStandardOutput = true;
-            proc1.StartInfo.CreateNoWindow = true;
-            proc1.Start();
-
-            proc2.StartInfo.FileName = @"..\TestConsoleApp2\bin\Debug\TestConsoleApp2.exe";
-            proc2.StartInfo.UseShellExecute = false;
-            proc2.StartInfo.RedirectStandardInput = true;
-            proc2.StartInfo.RedirectStandardOutput = true;
-            proc2.StartInfo.CreateNoWindow = true;
-            proc2.Start();
 
         }
 
@@ -53,12 +38,31 @@ namespace TheChessBoard
 
         private void btnConfirm_Click(object sender, EventArgs e)
         {
-            if(proc1 != null && proc2 != null)
+            proc1 = new Process();
+            proc2 = new Process();
+            proc1.StartInfo.FileName = @"..\..\..\TestConsoleApp1\bin\Debug\TestConsoleApp1.exe";
+            proc1.StartInfo.UseShellExecute = false;
+            proc1.StartInfo.RedirectStandardInput = true;
+            proc1.StartInfo.RedirectStandardOutput = true;
+            proc1.StartInfo.CreateNoWindow = true;
+            proc1.Start();
+
+            proc2.StartInfo.FileName = @"..\..\..\TestConsoleApp2\bin\Debug\TestConsoleApp2.exe";
+            proc2.StartInfo.UseShellExecute = false;
+            proc2.StartInfo.RedirectStandardInput = true;
+            proc2.StartInfo.RedirectStandardOutput = true;
+            proc2.StartInfo.CreateNoWindow = true;
+            proc2.Start();
+
+            if (proc1 != null && proc2 != null)
             {
                 proc2.StandardInput.WriteLine(txbInput2.Text);
                 txbOut1.Text = proc1.StandardOutput.ReadToEnd();
                 txbOut2.Text = proc2.StandardOutput.ReadToEnd();
             }
+
+            proc1.WaitForExit();
+            proc2.WaitForExit();
         }
     }
 }
